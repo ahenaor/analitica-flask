7af22f2b0e9fe5040e1d61138d199ca0d769213a
Alejandro Henao Ruiz <ahenaor@unal.edu.co>
2022-11-09 00:07:55 -0500

    Test conn a base de datos, ok en local

--- app.py
+++ app.py
@@ -1,29 +1,63 @@
 from datetime import datetime
+import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+from decouple import config
+import json
+
+
+from threading import Lock
+import socket
+import pyodbc
+from tenacity import *
+
 app = Flask(__name__)
 
+def connDB(data):
+	
+
+	cnxn = pyodbc.connect(config('SQLAZURECONNSTR_WWIF'))
+
+	cursor = cnxn.cursor()
+	
+	sql = 	"""
+			INSERT INTO dbo.Flask01 (primerInput) 
+			VALUES (?) 
+			"""
+	jsonData = {}
+	jsonData["primerInput"] = data
+
+
+	#cursor.execute(sql, json.dumps(jsonData))
+	cursor.execute(sql, 'CURRENT_TIMESTAMP')
+	
+	cursor.commit() 
+	cnxn.close()
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+	return send_from_directory(os.path.join(app.root_path, 'static'),
+		'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+	name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+	if name:
+		print('Request for hello page received with name=%s' % name)
+
+		connDB(name)
+
+		return render_template('hello.html', name = name)
+	else:
+		print('Request for hello page received with no name or blank name -- redirecting')
+		return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-   app.run()
+	app.run(debug= True)
